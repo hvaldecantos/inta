b0317c9ad38666882234a1feba0da8008074cb4b
Héctor Adrián Valdecantos <hvaldecantos@gmail.com>
2013-06-11 16:20:45 -0300

    Add a title according to the page visited

--- spec/features/paginas_estaticas_spec.rb
+++ spec/features/paginas_estaticas_spec.rb
@@ -7,6 +7,11 @@ describe "Paginas Estaticas" do
       visit '/paginas_estaticas/inicio'
       page.should have_content('INTA')
     end
+
+    it "should have the right title" do
+      visit '/paginas_estaticas/inicio'
+      page.should have_title "Sitio Web INTA Famaillá | Inicio"
+    end
   end
 
   describe "Pagina de Ayuda" do
@@ -14,12 +19,22 @@ describe "Paginas Estaticas" do
       visit '/paginas_estaticas/ayuda'
       page.should have_content('Ayuda')
     end
+
+    it "should have the right title" do
+      visit '/paginas_estaticas/ayuda'
+      page.should have_title "Sitio Web INTA Famaillá | Ayuda"
+    end
   end
 
   describe "Pagina Acerca" do
     it "should have the content 'Acerca'" do
       visit '/paginas_estaticas/acerca'
       page.should have_content('Acerca')
+    end
+
+    it "should have the right title" do
+      visit '/paginas_estaticas/acerca'
+      page.should have_title "Sitio Web INTA Famaillá | Acerca"
     end
   end
 end

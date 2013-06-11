11200f2c410fc025bc88ffb03e03f3956e36b0a4
Héctor Adrián Valdecantos <hvaldecantos@gmail.com>
2013-06-11 15:57:41 -0300

    Add static pages: inicio, ayuda

---
+++ spec/features/paginas_estaticas_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "Paginas Estaticas" do
+  
+  describe "Pagina de Inicio" do
+    it "should have the content 'INTA'" do
+      visit '/paginas_estaticas/inicio'
+      page.should have_content('INTA')
+    end
+  end
+
+  describe "Pagina de Ayuda" do
+    it "should have the content 'Ayuda'" do
+      visit '/paginas_estaticas/ayuda'
+      page.should have_content('Ayuda')
+    end
+  end
+end

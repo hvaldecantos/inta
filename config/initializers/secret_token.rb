ed252ed299cf0a532daa09b187ec7188eab05b68
Héctor Adrián Valdecantos <hvaldecantos@gmail.com>
2013-06-10 21:07:12 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inta::Application.config.secret_token = 'ec683a45dc28ad98aa6833e1cf53b744d2c1e76ac6df8380a5f89836b767b7c3dd1dea62f86f240130f000231bae09f4d3a95e985519249c1312bb28a805b6c4'

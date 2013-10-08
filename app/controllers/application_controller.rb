7abd1b4f0d0d49c495d867773edc20b1f3626c99
Héctor Adrián Valdecantos <hvaldecantos@gmail.com>
2013-10-08 15:48:47 -0300

    Add rescue_from to catch ActiveRecord::DeleteRestrictionError exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,7 @@ class ApplicationController < ActionController::Base
     redirect_to root_url, :alert => exception.message
   end
   
+  rescue_from ActiveRecord::DeleteRestrictionError do |exception|
+    redirect_to :back, :alert => exception.message
+  end
 end

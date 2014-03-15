f886c6095341e498543bdf2467d4b50129bd33bf
Héctor Adrián Valdecantos <hvaldecantos@gmail.com>
2014-03-14 21:20:51 -0300

    Add validation for AnalisisRsd on productor_id and cania_variedad_id

--- app/models/analisis_rsd.rb
+++ app/models/analisis_rsd.rb
@@ -29,7 +29,7 @@
 class AnalisisRsd < ActiveRecord::Base
   attr_accessible :agente_id, :analizado, :anio_plantacion, :comuna_municipio_id, :departamento_id, :fecha_analisis, :fecha_extraccion, :fecha_ingreso, :incidencia, :laboratorista_id, :localidad_id, :membrana, :paraje_id, :parcela_id, :procedencia_id, :productor_id, :promotor_id, :cania_variedad_id
 
-  validates_presence_of :comuna_municipio_id, :departamento_id, :paraje_id, :localidad_id, :agente_id, :promotor_id, :laboratorista_id
+  validates_presence_of :comuna_municipio_id, :departamento_id, :paraje_id, :localidad_id, :agente_id, :promotor_id, :laboratorista_id, :productor, :cania_variedad_id
   
   belongs_to :paraje
   belongs_to :departamento
